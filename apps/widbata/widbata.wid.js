b61c5646c439d1caf32368b48ccaa1571d0a35ff
ash <ash@sorrel.sh>
2022-07-25 02:29:57 +0100

    widbata: show green highlight when battery is full

--- apps/widbata/widbata.wid.js
+++ apps/widbata/widbata.wid.js
@@ -7,17 +7,19 @@ WIDGETS["bata"]={area:"tr",sortorder:-10,width:27,draw:function() {
   var s = 26;
   var t = 13; // thickness
   var x = this.x, y = this.y + 5;
+  let bat = E.getBattery();
   g.reset();
   g.setColor(g.theme.fg);
   g.fillRect(x,y+2,x+s-4,y+2+t); // outer
   g.clearRect(x+2,y+2+2,x+s-4-2,y+2+t-2); // centre
   g.setColor(g.theme.fg);
   g.fillRect(x+s-3,y+2+(((t - 1)/2)-1),x+s-2,y+2+(((t - 1)/2)-1)+4); // contact
-  g.fillRect(x+3, y+5, x +4 + E.getBattery()*(s-11)/100, y+t-1); // the level
+  g.fillRect(x+3, y+5, x +4 + bat*(s-11)/100, y+t-1); // the level
   if (Bangle.isCharging()) {
+    if (bat === 100) g.setColor('#0f0');
     for (let off = -1; off <= 1; off++) {
       let Y = y + off;
-      g.setColor(off === 0 ? '#000' : '#fff');
+      if (bat !== 100) g.setColor(off === 0 ? '#000' : '#fff');
       g.drawPoly([x+5,Y+9, x+11,Y+7, x+11,Y+10, x+17,Y+8], false);
     }
     g.setPixel(x+4, y+7);
