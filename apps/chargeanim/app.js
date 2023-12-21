65c5ed72b338e1c30de09c5c00f9b7299cc9d142
ash <ash@sorrel.sh>
2023-12-21 17:12:40 +0000

    chargeanim: revert quiet mode changes

--- apps/chargeanim/app.js
+++ apps/chargeanim/app.js
@@ -31,10 +31,5 @@ setInterval(anim,20);
 
 Bangle.on("charging", isCharging => {
   Bangle.setBacklight(1);
-  if (!isCharging) {
-    let settings = require("Storage").readJSON("setting.json", true) || {};
-    settings.quiet = 0;
-    require("Storage").writeJSON("setting.json", settings);
-    load();
-  }
+  if (!isCharging) load();
 });
--- apps/chargeanim/boot.js
+++ apps/chargeanim/boot.js
@@ -1,9 +1,4 @@
 Bangle.on("charging", isCharging => {
   Bangle.setBacklight(1);
-  if (isCharging) {
-    let settings = require("Storage").readJSON("setting.json", true) || {};
-    settings.quiet = 1;
-    require("Storage").writeJSON("setting.json", settings);
-    load("chargeanim.app.js");
-  }
+  if (isCharging) load("chargeanim.app.js");
 });
